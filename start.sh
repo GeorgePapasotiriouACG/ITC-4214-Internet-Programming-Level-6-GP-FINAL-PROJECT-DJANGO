ce3ebb804626e8b4ada6d9a993db6cc278304bd3
GeorgePapasotiriouACG <105849187+GeorgePapasotiriouACG@users.noreply.github.com>
2026-04-03 13:31:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Running migrations..."
 python manage.py migrate
-python manage.py loaddata yourdata.json
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Starting server..."
 gunicorn eshop.wsgi:application --bind 0.0.0.0:$PORT --workers 2
